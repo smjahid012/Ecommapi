7076e917cca10d95005db16ee81c4125a90c2c7f
smjahid <smjahid012@gmail.com>
2019-02-21 08:53:45 +0600

    Stock,Rating,Total Price,Reviews Logic Done

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -18,8 +18,18 @@ class ProductResource extends JsonResource
             'name' => $this->name,
             'description' =>$this->detail,
             'price' => $this->price,
-            'stock' => $this->stock,
-            'discount' =>$this->discount
+            //Stock Logic
+            'stock' => $this->stock == 0 ? 'Out of Stock' : $this->stock,
+            'discount' =>$this->discount,
+            //Total Price Logic
+            //Total Price Will Be Minus of Discount Price
+            'totalPrice' => round((1 - ($this->discount/100)) * $this->price,2),
+            //Rating Logic
+            //If reviews count 0 then it will Undefined.Return Error
+            'rating' => $this->reviews->count() > 0 ? round($this->reviews->sum('star')/$this->reviews->count(),2) : 'No Rating Yet Given',
+            //Reviews Href 
+            'href' => route('reviews.index', $this->id)
+
         ];
     }
 }
